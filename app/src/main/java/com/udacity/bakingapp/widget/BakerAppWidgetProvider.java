5399cfe69bcb3cff41ced1d2bb3d5422f27aa89b
Girum.A <galemu00@gmail.com>
2018-08-07 21:02:33 -0400

    add pendingIntent to lounch MainActivity when widget is clicked.

--- app/src/main/java/com/udacity/bakingapp/widget/BakerAppWidgetProvider.java
+++ app/src/main/java/com/udacity/bakingapp/widget/BakerAppWidgetProvider.java
@@ -1,11 +1,14 @@
 package com.udacity.bakingapp.widget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
 
 import com.udacity.bakingapp.R;
+import com.udacity.bakingapp.ui.MainActivity;
 
 /**
  * Implementation of App Widget functionality.
@@ -15,14 +18,18 @@ public class BakerAppWidgetProvider extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
+
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.baker_app_widget);
 
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0 , intent, 0);
 
+        views.setOnClickPendingIntent(R.id.widget_dounat_image, pendingIntent);
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
+
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
